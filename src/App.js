d91c558a4127ddbe2279da8406a130757511495f
tigerFish29 <paulwchabz28@gmail.com>
2022-08-28 20:40:33 +0100

    updated the fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import axios from 'axios';
+import './App.css';
 
 
 const App = () => {
@@ -21,19 +22,19 @@ const App = () => {
 
         <>
             
-            <button type="button"  className="btn btn-outline-primary" onClick={(e) => makeRequest(e)}>Click For Popular Movies</button>
+            <button type="button"  className="btn btn-outline-primary btn-lg" onClick={(e) => makeRequest(e)}>Click For Popular Movies</button>
 
             {data.map((movie, index) => (
                 <div  key={index} className="container text-center  movie-container">
                     <div  className="row align-items-start">
                         <div className="col">
 
-                            <h4 >Movie Title: {movie.original_title}</h4>
-                            <h5 >Movie id: {movie.id}</h5>
-                            <h5>Language:  {movie.original_language}</h5>
-                            <p >Overview: {movie.overview}</p>
-                            <p >Popularity: {movie.popularity}</p>
-                            <p>Date: {movie.release_date}</p>
+                            <h4 className="display-4">Movie Title: {movie.original_title}</h4>
+                            <h5 className="display-5">Movie id: {movie.id}</h5>
+                            <h5 className="display-5">Language:  {movie.original_language}</h5>
+                            <p className="lead">Overview: {movie.overview}</p>
+                            <p className="blockquote">Popularity: {movie.popularity}</p>
+                            <p className="blockquote">Date: {movie.release_date}</p>
                             <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="Movie poster" width="100" height="200"></img>
                         </div>
                     </div>
